0169ad7168dd26dcb7b97755cff4ad2d66266373
Tomáš Pitro <pit.tom@seznam.cz>
2023-08-20 00:58:17 +0200

    základ webu

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="cs">
 <head>
-    <title>Projekty</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Můj PHP web</title>
+    <style>
+        /* Základní styly pro záhlaví */
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+
+        /* Zarovnání textu v hlavní části obsahu */
+        main {
+            text-align: center;
+            margin: 2rem;
+        }
+    </style>
+    <script>
+        // Jednoduché JavaScriptové upozornění při načtení stránky
+        window.onload = function() {
+            alert("Vítejte na mé webové stránce!");
+        };
+    </script>
 </head>
 <body>
-
-<?php
-$seznam = ["PHP11111", "JS", "TYPESCRIPT"];
-echo implode(" ", $seznam);
-?>
-
+    <header>
+        <h1>Vítejte na mé PHP webové stránce</h1>
+    </header>
+    <main>
+        <?php
+        echo "<p>Tento obsah je generován pomocí PHP.</p>";
+        ?>
+    </main>
 </body>
 </html>
