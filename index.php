1af02bbe7d1230afdb5080e59b3d6e4698438ff7
Tomáš Pitro <pit.tom@seznam.cz>
2023-08-19 16:35:56 +0200

    vytvořen index.php a přidán seznam

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Projekty</title>
+</head>
+<body>
+
+<?php
+$seznam = ["projekt1", "projekt2", "projekt3"];
+echo implode(" ", $seznam);
+?>
+
+</body>
+</html>
